d02e61c012bbf988f84fb58af90d251bf6afd3c3
viditpant2024 <166274933+viditpant2024@users.noreply.github.com>
2025-08-30 23:18:51 +0530

    Script to load the model and calculate eval metrics

---
+++ loader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import joblib
+from PIL import Image
+from data.config import thresholds
+from sklearn.metrics import (accuracy_score,
+                             precision_score,
+                             recall_score,
+                             f1_score,
+                             roc_auc_score)
+
+data = pd.read_csv('datasets/diabetes.csv')
+X = data[['Pregnancies', 'Glucose', 'Insulin', 'BMI', 'Age']]
+y = data['Outcome']
+
+page_icon = Image.open("image/page_icon.jpg")
+
+model = joblib.load('model.pkl')
+
+
+y_score = model.predict_proba(X)[:, 1]
+y_pred = (y_score >= thresholds).astype(int)
+
+# Accuracy Score
+accuracy_result = round(accuracy_score(y, y_pred) * 100, 2)
+# F1 Score
+f1_result = (f1_score(y, y_pred) * 100).round(2)
+# Recall Score
+recall_result = (recall_score(y, y_pred) * 100).round(2)
+# Precision Score
+precision_result = (precision_score(y, y_pred) * 100).round(2)
+# ROC AUC Score
+roc_auc = (roc_auc_score(y, y_score)*100).round(2)
